7e2ebef1e07c2615ef53d39d9bee25861f594d12
artur_amaral <artur.amaral@mettainnovations.com.br>
2023-10-05 13:56:51 -0300

    Adicionando relatório.

--- selection/selection.c
+++ selection/selection.c
@@ -13,20 +13,22 @@ void swap(int *xp, int *yp)
 void selectionSort(int arr[], int n)
 {
     int i, j, min_idx;
- 
-    for (i = 0; i < n-1; i++)
+
+    for (i = 0; i < n-1; i++) 
     {
         min_idx = i;
         for (j = i+1; j < n; j++)
-          if (arr[j] < arr[min_idx])
-            min_idx = j;
- 
-           if(min_idx != i)
+        {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+
+        if(min_idx != i)
             swap(&arr[min_idx], &arr[i]);
+        
     }
 }
- 
-/* Function to print an array */
+
 void printArray(int arr[], int size)
 {
     int i;
@@ -34,14 +36,13 @@ void printArray(int arr[], int size)
         printf("%d ", arr[i]);
     printf("\n");
 }
- 
+
 // Driver program to test above functions
 int main()
 {
     int arr[] = {64, 34, 25, 12, 22, 11, 90, 3, 55, 78};
-    int n = 10;
+    int n = 10; //sizeof(arr)/sizeof(arr[0]);
     selectionSort(arr, n);
-    printf("Sorted array: \n");
     printArray(arr, n);
     return 0;
 }
